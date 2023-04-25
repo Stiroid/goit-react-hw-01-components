ba3892b2580c9f8c7317f26dc5a5322bc0ea5b93
Stiroid <stakhovsky89@gmail.com>
2023-04-25 23:31:43 +0300

    upd

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import data from './data.json';
+import css from './Statistics.module.css';
+
+export default function Statistics ({title, stats}) {
+    return (
+    <section className={css.Statistics}>
+    {title && (
+        <h2 className = {css.title}>{title}</h2>
+      )}
+       
+    <ul className={css.statList}>
+    {stats.map(({id, label, percentage}) => (
+          <li key={id} className ={css.item}>
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
+   
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired
+    }),
+  }
+  
+
+
